bc7ac5e8fd44598265a9c7c9320b37302c41ac39
pauleena phan <pauleena2002@gmail.com>
2023-07-08 12:56:22 -0700

    add taken sqr function and player turn display

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let body = document.getElementsByClassName("board");
+let turnStatus = document.getElementById("turn");
+let error = document.getElementById("error");
 
 //module
 const board = (()=>{
@@ -26,22 +28,33 @@ const player = (num, turn) =>{
 const playerOne = player(1, true);
 const playerTwo = player(2, false);
 
-
-console.log(playerOne.title, playerOne.turn);
-
-function play(player){
+function play(){
     //rotate by player
     if(playerOne.turn == true){
-        this.innerHTML = "x";
-        playerOne.turn = false;
-        playerTwo.turn = true;
+        //need to check if sqr is taken 
+        if(this.innerHTML != " "){
+            error.innerHTML = "the square you chose is taken please chose another one";
+        }
+        //check if sqr is empty
+        if(this.innerHTML == " "){
+            //when the sqr is not empty the player will be able to place a x or o
+            turnStatus.innerHTML = "Player 2's turn";
+            this.innerHTML = "x";
+            playerOne.turn = false;
+            playerTwo.turn = true;
+            error.innerHTML = " ";
+        }
     }else{
-        this.innerHTML = "o";
-        playerTwo.turn = false;
-        playerOne.turn = true;
+        if(this.innerHTML != " "){
+            error.innerHTML = "the square you chose is taken please chose another one";
+        }
+        if(this.innerHTML == " "){
+            turnStatus.innerHTML = "Player 1's turn";
+            this.innerHTML = "o";
+            playerTwo.turn = false;
+            playerOne.turn = true;
+            error.innerHTML = " ";
+        }
     }
-    
-    //check if the sqr is empty
-
-    //check for win condition
 }
+

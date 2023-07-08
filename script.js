82c3c7e9f96b7e05c3493eef5599e51200f21c41
pauleena phan <pauleena2002@gmail.com>
2023-07-08 12:07:15 -0700

    added players and player turns

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let body = document.getElementsByClassName("board");
 
-
+//module
 const board = (()=>{
-    let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let board = [" " ," " ," " ," " ," " ," " ," " ," " ," " ,];
     //let board = [["x", "o", "x"], [4,5,6], [7,8,9]];
 
 
@@ -12,7 +12,36 @@ const board = (()=>{
         sqr.className = "sqr";
         sqr.textContent = board[i];
         body[0].appendChild(sqr);
+        sqr.addEventListener("click", play);
     }
-
-    return board;
 })();
+
+//factories
+const player = (num, turn) =>{
+    const title = "player " + num;
+    
+    return {title, turn}
+}
+
+const playerOne = player(1, true);
+const playerTwo = player(2, false);
+
+
+console.log(playerOne.title, playerOne.turn);
+
+function play(player){
+    //rotate by player
+    if(playerOne.turn == true){
+        this.innerHTML = "x";
+        playerOne.turn = false;
+        playerTwo.turn = true;
+    }else{
+        this.innerHTML = "o";
+        playerTwo.turn = false;
+        playerOne.turn = true;
+    }
+    
+    //check if the sqr is empty
+
+    //check for win condition
+}
